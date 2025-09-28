abb67b5a4b6d6380eff27cd20a4356b121d803fb
Yagnik Ravrani <75234459+yagnik-android@users.noreply.github.com>
2025-09-28 09:31:55 +0530

    generate apk file

---
+++ keystore.sh
@@ -0,0 +1,47 @@
+keytool -genkey -v -keystore MantraJaap.keystore -alias MantraJaap -keyalg RSA -keysize 2048 -validity 10000
+MantraJaap
+
+yagnik@yagniks-Mac-mini MantraJaap % keytool -genkey -v -keystore MantraJaap.keystore -alias MantraJaap -keyalg RSA -keysize 2048 -validity 10000
+
+Enter keystore password:  
+Re-enter new password: 
+What is your first and last name?
+  [Unknown]:  MantraJaap
+What is the name of your organizational unit?
+  [Unknown]:  MantraJaap
+What is the name of your organization?
+  [Unknown]:  MantraJaap
+What is the name of your City or Locality?
+  [Unknown]:  MantraJaap
+What is the name of your State or Province?
+  [Unknown]:  MantraJaap
+What is the two-letter country code for this unit?
+  [Unknown]:  IN        
+Is CN=MantraJaap, OU=MantraJaap, O=MantraJaap, L=MantraJaap, ST=MantraJaap, C=IN correct?
+  [no]:  yes
+
+Generating 2,048 bit RSA key pair and self-signed certificate (SHA256withRSA) with a validity of 10,000 days
+        for: CN=MantraJaap, OU=MantraJaap, O=MantraJaap, L=MantraJaap, ST=MantraJaap, C=IN
+[Storing MantraJaap.keystore]
+yagnik@yagniks-Mac-mini MantraJaap % 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
